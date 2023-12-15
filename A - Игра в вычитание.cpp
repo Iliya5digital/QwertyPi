f278bc042ec3dc76d9c8304ef95277b1104f05d8
Iliya5digital <115736491+Iliya5digital@users.noreply.github.com>
2023-12-15 17:29:25 +0300

    Create A - Игра в вычитание.cpp

---
+++ A - Игра в вычитание.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+// failed IMO, only got Bronze
+// At least give me some CF rating
+
+using namespace std;
+
+void solve(){
+    int a, b; cin >> a >> b;
+    cout << a + b << endl;
+}
+
+int32_t main(){
+    int t = 1; cin >> t;
+    while(t--){
+        solve();
+    }
+}
